fc186cbc612cb8af7e378ffcf811c9865e011e2c
oskar338 <oskar.steinke137@gmail.com>
2023-08-30 23:58:48 +0200

    k

--- gzuz187.php
+++ gzuz187.php
@@ -8,40 +8,30 @@
   <body>
     <?php
 
-    if(isset($_POST["submit"])){
-      require("mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+if(isset($_POST["submit"])){
+  require("mysql.php");
+  $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+  $stmt->bindParam(":user", $_POST["username"]);
+  $stmt->execute();
+  $count = $stmt->rowCount();
+  if($count == 0){
+    //Username ist frei
+    if($_POST["pw"] == $_POST["pw2"]){
+      //User anlegen
+      $stmt = $mysql->prepare("INSERT INTO accounts (USERNAME, PASSWORD) VALUES (:user, :pw)");
       $stmt->bindParam(":user", $_POST["username"]);
+      $hash = password_hash($_POST["pw"], PASSWORD_BCRYPT);
+      $stmt->bindParam(":pw", $hash);
       $stmt->execute();
-      $count = $stmt->rowCount();
-      if($count == 0){
-        //Username ist frei
-        $stmt = $mysql->prepare("SELECT * FROM accounts WHERE EMAIL = :email"); //Username überprüfen
-        $stmt->bindParam(":email", $_POST["email"]);
-        $stmt->execute();
-        $count = $stmt->rowCount();
-        if($count == 0){
-          if($_POST["pw"] == $_POST["pw2"]){
-            //User anlegen
-            $stmt = $mysql->prepare("INSERT INTO accounts (USERNAME, PASSWORD, EMAIL, TOKEN) VALUES (:user, :pw, :email, null)");
-            $stmt->bindParam(":user", $_POST["username"]);
-            $hash = password_hash($_POST["pw"], PASSWORD_BCRYPT);
-            $stmt->bindParam(":pw", $hash);
-            $stmt->bindParam(":email", $_POST["email"]);
-            $stmt->execute();
-            echo "Dein Account wurde angelegt";
-          } else {
-            echo "Die Passwörter stimmen nicht überein";
-          }
-        } else {
-          echo "Email bereits vergeben";
-        }
-      } else {
-        echo "Der Username ist bereits vergeben";
-      }
+      echo "Dein Account wurde angelegt";
+    } else {
+      echo "Die Passwörter stimmen nicht überein";
     }
-    
-     ?>
+  } else {
+    echo "Der Username ist bereits vergeben";
+  }
+}
+ ?>
     <div class="center">
     <h1>Account erstellen</h1>
     <form action="gzuz187.php" method="post">
