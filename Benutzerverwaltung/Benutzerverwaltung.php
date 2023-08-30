28b3dd314d4d161b3670b5b60b7f6226b8f88254
oskar338 <oskar.steinke137@gmail.com>
2023-08-31 00:37:52 +0200

    g

--- Benutzerverwaltung/Benutzerverwaltung.php
+++ Benutzerverwaltung/Benutzerverwaltung.php
@@ -19,7 +19,7 @@
 
     session_start();
     if(!isset($_SESSION["username"])){
-    header("Location: ../index.html");
+    header("Location: index.php");
     exit;
     }
 
--- geheim/geheimimage.php
+++ geheim/geheimimage.php
@@ -3,7 +3,7 @@
 <?php
 session_start();
 if(!isset($_SESSION["username"])){
-  header("Location: index.html");
+  header("Location: index.php");
   exit;
 }
 
