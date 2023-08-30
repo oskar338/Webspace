fb0ff9f6684d60b7e9f2146e8a12a8866769b0d6
oskar338 <oskar.steinke137@gmail.com>
2023-08-30 23:37:20 +0200

    Benutzerverwaltung

---
+++ Benutzerverwaltung/user.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Benutzerverwaltung</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
+</head>
+<body>
+<table>
+    <tr>
+    <th>ID</th>
+    <th>Username</th>
+    <th>erster Login</th>
+    <th>letzter Login</th>
+    <th>Aktionen</th>
+    </tr>
+
+    <?php
+    require("mysql.php");
+
+    if(isset($_GET["del"])){
+        if(!empty($_GET["del"])){
+            $stmt = $mysql->prepare("DELETE FROM users WHERE ID = :id");
+            $stmt->execute(array(":id" => $_GET["del"]));
+            ?>
+            <p>Der Benutzer wurde gelöscht</p>
+            <?php
+        }
+    }
+
+    $stmt = $mysql->prepare("SELECT * FROM users");
+    $stmt->execute();
+    while($row = $stmt->fetch()){
+        ?>
+        <tr>
+        <td><?php echo $row["ID"] ?></td>
+        <td><?php echo $row["USERNAME"] ?></td>
+        <td><?php echo date("d.M.Y - H:i", $row["LASTLOGIN"]) ?></td>
+        <td><?php echo date("d.M.Y - H:i", $row["FIRSTLOGIN"]) ?></td>
+        <td><a href="user.php?del=<?php echo $row["ID"] ?>"><i class="fas fa-user-minus"></i></a></td>
+
+        </tr>
+        <?php
+    }
+    ?>
+
+    
+    </table>
+   
+    <a href="index.php">Zurück zur Starseite</a><br>
+    <a href="register.php">Neuen Account erstellen</a><br>
+    <a href="logout.php">Auslogen</a><br>
+
+</body>
+</html>
---
+++ Benutzerverwaltung/verwaltung.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login Benutzerverwaltung</title>
+    <link rel="stylesheet" href="/style/stylelogin.css">
+  </head>
+  <body>
+    <?php
+    if(isset($_POST["submit"])){
+      require("mysql.php");
+      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+      $stmt->bindParam(":user", $_POST["username"]);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+
+      if($count == 1){
+        //Username ist frei
+        $row = $stmt->fetch();
+
+        if(password_verify($_POST["pw"], $row["PASSWORD"])){
+          session_start();
+          $_SESSION["username"] = $row["USERNAME"];
+          header("Location: user.php");
+
+        } else {
+          echo "Der Login ist fehlgeschlagen";
+        }
+
+      } else {
+          echo "Der Login ist fehlgeschlagen";
+      }
+    }
+     ?>
+    <div class="center">
+
+    <h1>Anmelden</h1>
+    <form action="index.php" method="post">
+
+    <div class="txt_field">
+      <input type="text" name="username" placeholder="Username" required><br>
+    </div>
+
+    <div class="txt_field">
+      <input type="password" name="pw" placeholder="Passwort" required><br>
+    </div>
+
+    <div class="signup_link">
+      <button type="submit" name="submit">Einloggen</button>
+      </div>
+
+    </form>
+    </div>
+
+    <br>
+
+  </body>
+
+</html>
