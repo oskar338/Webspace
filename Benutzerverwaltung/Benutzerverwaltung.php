0b6582393a527493dc09698587f1c48be29f08f9
oskar338 <oskar.steinke137@gmail.com>
2023-09-01 15:40:35 +0200

    Update Benutzerverwaltung.php

--- Benutzerverwaltung/Benutzerverwaltung.php
+++ Benutzerverwaltung/Benutzerverwaltung.php
@@ -24,7 +24,7 @@
 
     require("rankmanger.php");
     if(getRank($_SESSION["username"]) != ADMIN){
-    header("Location: geheim.php");
+    header("Location: AdminLogin.php");
     exit;
     }
 
