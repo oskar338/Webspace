e016bcb9867db30b143e6bf92ab651684ad6449f
Oskar <oskar.steinke137@gmail.com>
2023-10-30 10:30:07 +0100

    Update viewimage.php

--- Benutzerverwaltung/viewimage.php
+++ Benutzerverwaltung/viewimage.php
@@ -15,7 +15,7 @@ if(isset($_GET["id"])){
         $stmt = $mysql->prepare("SELECT * FROM images WHERE id = :id");
         $stmt->execute(array(":id" => $_GET["id"]));
         $row = $stmt->fetch();
-        $image_url = $_SERVER['DOCUMENT_ROOT'] . "/geheim/uploads/" . $row["image_url"];
+        $image_url = "suffkopp.com/httpdocs/geheim/uploads/" . $row["image_url"];
         
         // Foto anzeigen
         echo "<img src=\"$image_url\">";
