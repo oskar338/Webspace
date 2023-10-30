8646402e13f289961d1469956ab9c356e44cb59c
Oskar <oskar.steinke137@gmail.com>
2023-10-30 12:24:11 +0100

    Update Benutzerverwaltung.php

--- Benutzerverwaltung/Benutzerverwaltung.php
+++ Benutzerverwaltung/Benutzerverwaltung.php
@@ -57,7 +57,7 @@
    
     <a href="register.php">Neuen Account erstellen</a><br>
     <a href="../index.php">Zurück zur Starseite</a><br>
-    <a href="deleteimage.php">Foto löschen</a><br>
+    <a href="/Benutzerverwaltung/ImageVerwaltung/deleteimage.php">Foto löschen</a><br>
 
 </body>
 </html>
