aa8f07c1b68b1a398b04770b7ce496338471968c
Oskar <oskar.steinke137@gmail.com>
2023-10-30 09:19:51 +0100

    Update deleteimage.php

--- Benutzerverwaltung/deleteimage.php
+++ Benutzerverwaltung/deleteimage.php
@@ -36,7 +36,7 @@
 require("mysql.php");
     if(isset($_GET["del"])){
         if(!empty($_GET["del"])){
-            $stmt = $mysql->prepare("DELETE FROM images WHERE ID = :id");
+            $stmt = $mysql->prepare("DELETE FROM images WHERE id = :id");
             $stmt->execute(array(":id" => $_GET["del"]));
             ?>
             <p>Das Bild wurde gelöscht</p>
