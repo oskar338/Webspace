d6e9b8ebb17e95a24f45c2cff680d3112a784cb9
Oskar <oskar.steinke137@gmail.com>
2023-10-30 10:40:40 +0100

    Update viewimage.php

--- Benutzerverwaltung/viewimage.php
+++ Benutzerverwaltung/viewimage.php
@@ -1,1 +1,18 @@
+<?php
+require("mysql.php");
 
+if(isset($_GET["id"])){
+    if(!empty($_GET["id"])){
+        $stmt = $mysql->prepare("SELECT * FROM images WHERE id = :id");
+        $stmt->execute(array(":id" => $_GET["id"]));
+        $row = $stmt->fetch();
+        $image_url = "/httpdocs/geheim/uploads/" . $row["image_url"];
+        
+        // Foto anzeigen
+        echo "<img src=\"$image_url\">";
+    }
+}
+?>
+    
+</body>
+</html>
