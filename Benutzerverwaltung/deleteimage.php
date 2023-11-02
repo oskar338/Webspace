2149361a1acfd4a1556b924dcc10a36bf64176f6
oskar338 <oskar.steinke137@gmail.com>
2023-11-02 17:39:09 +0100

    Update deleteimage.php

--- Benutzerverwaltung/deleteimage.php
+++ Benutzerverwaltung/deleteimage.php
@@ -64,7 +64,7 @@ $stmt = $mysql->prepare("SELECT * FROM images ORDER BY id DESC");
         ?>
         <tr>
         <td><?php echo $row["id"] ?></td>
-        <td><?php echo $image_url; ?>"><img src="<?php echo $image_url; ?>"></td>
+        <td><a href="<?php echo $row["image_url"] ?>"><img src="<?php echo $row["image_url"] ?>"></a></td>
         <td><a href="deleteimage.php?del=<?php echo $row["id"] ?>"><i class="fas fa-user-minus"></i></a></td>
         </tr>
         <?php
